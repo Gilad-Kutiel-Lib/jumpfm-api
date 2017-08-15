0ac8438de1135130a36016e5010c2b8ad2dea377
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-15 07:50:09 +0300

    fixed gist

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
 
+export interface Dialog {
+    open(dialog: {
+        onDialogOpen: (input: HTMLInputElement) => void
+    }): void
+}
+
 export interface Settings {
     getStr(key: string, defaultValue: string): string
 }
 
+export interface Item {
+
+}
+
+export interface Panel {
+    getSelectedItemsPaths(): string[]
+}
+
 export interface StatusBar {
-    info(key: string, txt: string): void
+    info(key: string, txt: string, clearTimeout?: number): void
 }
 
 export interface JumpFm {
     readonly statusBar: StatusBar
     readonly root: string
     readonly settings: Settings
+    readonly dialog: Dialog
+
+    opn(path: string): void
+    bindKeys(name: string, keys: string[], action: () => void): {
+        filterMode: () => void
+    }
+    getActivePanel(): Panel
 }
