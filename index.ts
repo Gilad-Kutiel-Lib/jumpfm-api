03c328661d1a13d4420e0b2e565cecca8c547e7c
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-15 07:37:06 +0300

    statusbar + settings

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-export abstract class JumpFm {
-    hello(name: string) { }
+
+export interface Settings {
+    getStr(key: string, defaultValue: string): string
+}
+
+export interface StatusBar {
+    info(key: string, txt: string): void
+}
+
+export interface JumpFm {
+    readonly statusBar: StatusBar
+    readonly root: string
+    readonly settings: Settings
 }
